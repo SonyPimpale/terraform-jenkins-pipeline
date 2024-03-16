4dcb3a5443fcf9a12ea695c3771bf82f361eeed3
SonyPimpale <108893257+SonyPimpale@users.noreply.github.com>
2024-03-16 11:21:38 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,15 @@
 
+provider "aws" {
+  region = var.aws_region
+}
+
+
+
+# Create AWS ec2 instance
+resource "aws_instance" "myFirstInstance" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  tags= {
+    Name = var.tag_name
+  }
+}
