3fadcaf82d892c1e207c0faf565cf1e020793715
SonyPimpale <108893257+SonyPimpale@users.noreply.github.com>
2024-03-19 21:54:45 +0000

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,14 @@
+terraform {
+ required_providers {
+   aws = {
+     source  = "hashicorp/aws"
+     version = "~> 4.18.0"
+   }
+ }
+
+ backend "s3" {
+   bucket = "terraform-state"
+   key    = "state"
+   region = "eu-west-2"
+ }
+}
