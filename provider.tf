fe71b8e202d067ca07dfef1a356a1cc02024f0da
SonyPimpale <108893257+SonyPimpale@users.noreply.github.com>
2024-03-20 10:47:00 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,14 +1,1 @@
-terraform {
- required_providers {
-   aws = {
-     source  = "hashicorp/aws"
-     version = "~> 4.18.0"
-   }
- }
 
- backend "s3" {
-   bucket = "jc-tf-test-bucket1"
-   key    = "state"
-   region = "eu-west-2"
- }
-}
